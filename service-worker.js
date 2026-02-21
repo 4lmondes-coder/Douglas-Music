6f0be03dcb59e8a03a11eb54a8cd4eff8ec49eaf
4lmondes-coder <4lmondes@gmail.com>
2026-02-20 23:30:10 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'douglas-music-v2';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
