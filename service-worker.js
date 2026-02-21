cab5d049ca63697484d174fa7c5623e9fc06f11c
4lmondes-coder <4lmondes@gmail.com>
2026-02-21 00:21:07 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,13 @@
-const CACHE_NAME = "douglas-player-v1";
+const CACHE_NAME = "douglas-music-v1";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "https://cdn.jsdelivr.net/npm/jsmediatags@3.9.7/dist/jsmediatags.min.js"
-];
-
-self.addEventListener("install", event=>{
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event=>{
+self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", event=>{
-  event.respondWith(
-    caches.match(event.request).then(r=>r||fetch(event.request))
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
